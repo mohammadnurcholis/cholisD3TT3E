80cf7536e3157e70de9d8cadadee5a15a8eb534a
mohammadnurcholis <42969313+mohammadnurcholis@users.noreply.github.com>
2018-09-06 20:58:21 +0700

    cholis
    tugas pak ardi1

---
+++ tugas pak ardi1/src/tugas/pak/ardi1/TugasPakArdi1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pak.ardi1;
+
+/**
+ *
+ * @author user
+ */
+public class TugasPakArdi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int i,x,jumlah = 5;
+        for(i=1;i<=jumlah;i++){
+            for(x=1;x<=i;x++){
+            System.out.print("*");}
+        System.out.println("");
+        }
+    }
+    
+}
