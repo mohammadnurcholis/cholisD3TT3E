64e2734d9780b7d5ac7119b065f9825711193130
mohammadnurcholis <42969313+mohammadnurcholis@users.noreply.github.com>
2018-09-19 19:49:25 +0700

    tugas 2

---
+++ tugas2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pak.ardi2;
+
+/**
+ *
+ * @author user
+ */
+public class tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 3;
+
+        for (int i = 1; i <= jumlah; i++) {
+            for (int f = 1; f <= jumlah; f++) {
+                if (f == 1 || f == jumlah || i == 1 || i == jumlah) {
+                    System.out.print("#");
+                } else {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println("");
+
+        }
+    }
+
+}
